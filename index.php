401e8f89d33ab90982b7b7ceb38620bc77bdf79a
gscsdlz <lz842063523@foxmail.com>
2017-03-06 12:47:39 +0800

    取消require_once

--- index.php
+++ index.php
@@ -3,17 +3,18 @@ $stime = microtime ( true );
 session_start();
 date_default_timezone_set("PRC");
 define ( "APPPATH", dirname ( __FILE__ ) );
-require_once 'Include/router.class.php';
-require_once 'Include/WORK.class.php';
+
+require 'Include/router.class.php';
+require 'Include/WORK.class.php';
 $router = new router ();
 $controlClass = $router->control;
 $action = $router->action;
 if (file_exists ( 'Include/' . $controlClass . '.php' )) {
-	require_once 'Include/' . $controlClass . '.php';
+	require 'Include/' . $controlClass . '.php';
 	$control = WORK::create ( $controlClass );
 	$control->$action ();
 } else {
-	require_once 'Include/indexControl.php';
+	require 'Include/indexControl.php';
 	$control = new indexControl ();
 	$control->index ();
 }
