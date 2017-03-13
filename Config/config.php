0547850a1f9395348ef4890085a8c4bae0619c3d
gscsdlz <lz842063523@foxmail.com>
2017-03-13 22:45:50 +0800

    修改一页显示的题目数

--- Config/config.php
+++ Config/config.php
@@ -6,7 +6,7 @@ $dbInfo = array (
 		'dbpass' => '1234' 
 );
 define ( 'LOGINTIMEOUT', 6 * 60 * 60 ); // 5小时 ACM标准比赛时间
-define ( 'PROBLEMPAGEMAXSIZE', 50 ); // 一页显示的题目数
+define ( 'PROBLEMPAGEMAXSIZE', 100 ); // 一页显示的题目数
 define ( 'STATUSPAGEMAXSIZE', 20 ); // 一页显示的状态数
 
 $contest = false;//定义是否是比赛模式 主要用于调整用户界面
